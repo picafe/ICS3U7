fc0e3b10e75021856280ff23fddb0cb75b72e59c
owopi <awesomebuilds.playmc@gmail.com>
2022-10-31 07:29:54 -0400

    updated Nim.java

--- src/Nim.java
+++ src/Nim.java
@@ -9,27 +9,42 @@ import java.util.*;
 public class Nim {
     static Scanner sc = new Scanner(System.in);
     public static void main (String[] args) {
-        int stones = (int) (Math.random() * 16) + 15;
-        while (stones > 0) {
-            System.out.print("There are " + stones + " stones. How many would you live to take (max of 3): ");
-            int input = sc.nextInt();
-            // calls function to test valid input
-            input = isValidEntry(input, stones);
-            // tests to see if you lost by taking the last stone
-            if (stones - input == 0) {
-                System.out.println("You lost by taking the last stone!");
-                break;
+        boolean play = true;
+        int wins = 0, loses = 0, games = 0;
+        while (play) {
+            int stones = (int) (Math.random() * 16) + 15;
+            while (stones > 0) {
+                System.out.print("There are " + stones + " stones. How many would you live to take (max of 3): ");
+                int input = sc.nextInt();
+                // calls function to test valid input
+                input = isValidEntry(input, stones);
+                // tests to see if you lost by taking the last stone
+                if (stones - input == 0) {
+                    System.out.println("You lost by taking the last stone!");
+                    loses += 1;
+                    break;
+                }
+                stones -= input;
+                // calls computer input function
+                int computer = drawStones(stones);
+                System.out.println("There are " + stones + " stones. The computer takes " + computer);
+                // tests to see if the computer lost by taking the last stone
+                if (stones - computer == 0) {
+                    System.out.println("You won!");
+                    wins += 1;
+                    break;
+                }
+                stones -= computer;
             }
-            stones -= input;
-            // calls computer input function
-            int computer = drawStones(stones);
-            System.out.println("There are " + stones + " stones. The computer takes " + computer);
-            // tests to see if the computer lost by taking the last stone
-            if (stones - computer == 0) {
-                System.out.println("You won!");
-                break;
+            games += 1;
+            System.out.print("Would you like to play again? (y or n) ");
+            char again = Character.toLowerCase(sc.next().charAt(0));
+            if (again == 'n') {
+                play = false;
+                System.out.println("Games: " + games);
+                System.out.println("Wins: " + wins);
+                System.out.println("Loses: " + loses);
             }
-            stones -= computer;
         }
     }
     public static int isValidEntry (int input, int stones) {
