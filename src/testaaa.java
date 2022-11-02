98cb6d53ca5e84c7126a14f19670a8d104d4dc95
owopi <awesomebuilds.playmc@gmail.com>
2022-11-02 07:25:36 -0400

    replaced test

--- src/testaaa.java
+++ src/testaaa.java
@@ -1,40 +1,17 @@
 import java.util.*;
 public class testaaa {
     public static void main(String[] args) {
-        ArrayList<String> songs = new ArrayList<String>();
-        Scanner sc = new Scanner (System.in);
-        int button = 0, num = 0;
-        String tmp;
-        songs.add("A");
-        songs.add("B");
-        songs.add("C");
-        songs.add("D");
-        songs.add("E");
-        while (button != 4) {
-            button = sc.nextInt();
-            num = sc.nextInt();
-            if (button == 1) {
-                for (int i = 0; i < num; i++) {
-                    songs.add(songs.get(0));
-                    songs.remove(0);
-                }
-            }
-            else if (button == 2) {
-                for (int i = 0; i < num; i++) {
-                    songs.add(0, songs.get(4));
-                    songs.remove(5);
-                }
-            }
-            else if (button == 3 ) {
-                for (int i = 0; i < num; i++) {
-                    songs.add(0, songs.get(1));
-                    songs.remove(2);
-                }
+        String str = notReplace("This is right");
+        System.out.println(str);
+    }
+    public static String notReplace(String str) {
+        str = " " + str + " ";
+        for (int i = 1; i < str.length()-1; i++) {
+            if (str.substring(i, i+2).equals("is")  && !Character.isLetter(str.charAt(i + 2)) && !Character.isLetter(str.charAt(i - 1))) {
+                str = str.substring(0, i) + "is not" + str.substring(i+2);
             }
         }
-        for (int j = 0; j < songs.size(); j++) {
-            System.out.print(songs.get(j) + " ");
-        }
+        return str.substring(1, str.length() - 1);
     }
 }
 
